df7dcbb821c2aa0c300911851de5a06e834a810c
Jemoh009 <kellyjames2550@gmail.com>
2026-09-24 20:02:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ total_price.addEventListener("click", () => {
   const total = calculateTotal();
   document.getElementById("subtotal").textContent = `Ksh ${total.toFixed(2)}`;
 
-  const taxRate = 0.16; // 16% tax rate
+  const taxRate = 0.18; // 18% tax rate
   const taxAmount = total * taxRate;
   document.getElementById("tax").textContent = `Ksh ${taxAmount.toFixed(2)}`;
 
   const grandTotal = total + taxAmount;
   document.getElementById("total").textContent = `Ksh ${grandTotal.toFixed(2)}`;
 
-  alert(`Product: ${product_name.value}\nUnit Price: Ksh ${product_price.value}\nQuantity: ${product_quantity.value}\nSubtotal: Ksh ${total.toFixed(2)}\nTax (16%): Ksh ${taxAmount.toFixed(2)}\nGrand Total: Ksh ${grandTotal.toFixed(2)}`);
+  
 });
